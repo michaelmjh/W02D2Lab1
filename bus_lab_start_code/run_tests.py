1fa58a3d06144efaa2f56f3eaa3f936ab48bc076
Nick Jones <nickj_24@hotmail.com>
2022-05-10 11:43:03 +0100

    update to lab

--- bus_lab_start_code/run_tests.py
+++ bus_lab_start_code/run_tests.py
@@ -1,7 +1,8 @@
 import unittest
 from tests.bus_test import TestBus
-from tests.bus_stop_test import TestBusStop
-from tests.person_test import TestPerson
 
-if __name__ == '__main__':
+# from tests.bus_stop_test import TestBusStop
+# from tests.person_test import TestPerson
+
+if __name__ == "__main__":
     unittest.main()
